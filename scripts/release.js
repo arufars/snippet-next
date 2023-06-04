a27a9f6f25a3c2696efea10c40f490a9159ff5c4
arufars <firselee@gmail.com>
2023-06-04 13:36:20 +0700

    add action github

---
+++ scripts/release.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const { exec, getExecOutput } = require("@actions/exec");
+
+const { version } = require("../package.json");
+
+const tag = `v${version}`;
+const releaseLine = `v${version.split(".")[0]}`;
+
+async function checkVersionPublished() {
+  try {
+    process.chdir(path.join(__dirname, ".."));
+    const { exitCode, stderr } = await getExecOutput(
+      `git`,
+      ["ls-remote", "--exit-code", "origin", "--tags", `refs/tags/${tag}`],
+      {
+        ignoreReturnCode: true,
+      }
+    );
+    if (exitCode === 0) {
+      console.log(`Action is not being published because version ${tag} is already published`);
+      return true;
+    }
+    if (exitCode !== 2) {
+      throw new Error(`git ls-remote exited with ${exitCode}:\n${stderr}`);
+    }
+    return false;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+
+async function publish() {
+  try {
+    process.chdir(path.join(__dirname, ".."));
+    await exec("git", ["checkout", "--detach"]);
+    await exec("git", ["add", "--force", "dist"]);
+    await exec("git", ["commit", "-m", tag]);
+    await exec("changeset", ["tag"]);
+    await exec("git", [
+      "push",
+      "--force",
+      "--follow-tags",
+      "origin",
+      `HEAD:refs/heads/${releaseLine}`,
+    ]);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+(async () => {
+  if (!(await checkVersionPublished())) {
+    await publish();
+  }
+})();
